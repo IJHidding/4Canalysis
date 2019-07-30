c4ce58992d6f94ce4eb54b6f75dfa2bce8d30b13
Hidding <i.j.hidding@umcg.nl>
2019-07-30 09:27:47 +0100

    added all files used in the pipeline

---
+++ 4CMappingScript.sh
@@ -0,0 +1,278 @@
+#!/bin/bash
+#SBATCH --job-name=135bp
+#SBATCH --time=11:59:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=40gb
+#SBATCH --nodes=1
+#SBATCH --open-mode=append
+#SBATCH --export=NONE
+#SBATCH --get-user-env=L
+
+
+##Lists the current location to find all files, can be used in an install function
+####Preset this to ensure proper working
+currentlocation=/groups/umcg-gdio/tmp04/umcg-ihidding/4C/peakC/PipelineWork
+Jaspardatafile=/groups/umcg-gdio/tmp04/umcg-ihidding/peakC/PipelineWork/Jasparbed/
+JasparMatrixInfo=/groups/umcg-gdio/tmp04/umcg-ihidding/peakC/PipelineWork/Jasparmatrix/
+
+
+#Help function, gives information on the used parameters and usability of the program
+###################################HELP AND INFORMATION########################################################
+
+function showHelp() {
+        #
+        # Display commandline help on STDOUT.
+        #
+	cat <<EOH
+===============================================================================================================
+Pipeline to demultiplex, align and analyze 4C data.
+Usage:
+	sh $(basename $0) OPTIONS
+Options:
+	-h	Show this help.
+	Required:
+	-i	inputfile (See readme for details)
+	Optional:
+	-w	Specify the size of the running window (default=300)
+	-l	Specify the steps taken in the running window (default=50)
+	-r	Runs the R installer to install the peakC package and required libraries
+===============================================================================================================
+EOH
+	trap - EXIT
+        exit 0
+}
+################################################################################################################
+#Prep and more prep, loading the required modules, sets the perl library to include inline::C and creates the output folder.
+################################################################################################################
+##Loads all required modules and adds the perl library to path.
+function Prepwork() {
+	#loading all the modules used in the prepwork and the mapping scripts 
+	module load cutadapt
+	module load BWA
+	module load PerlPlus
+	module load BEDTools
+	#Loads the perl library
+	export PERL5LIB=${currentlocation}/Requirements/perl5/lib/perl5/
+	if [ ! -d "${currentlocation}/4C_Output" ];
+	then
+		mkdir ${currentlocation}/4C_Output
+	fi
+
+	output=${currentlocation}/4C_Output
+	if [ ! -d "${output}/AnalysisOutput" ];
+	then
+	        mkdir ${output}/AnalysisOutput
+	fi
+
+	Jasparoutput=${output}/AnalysisOutput
+
+	PathToReferenceGenome=${currentlocation}/Requirements/
+
+	data=${currentlocation}/inputfiles
+
+	
+	#Determines the length of the reads in the file.
+	reads=$(awk 'NR==2' ${data}/*  | wc -m)
+	lengthofread=$((${reads} -1))
+
+	if [ ! -d "${output}/test_run/splitted_seq/" ]; 
+	then 
+		mkdir ${output}/test_run
+		mkdir ${output}/test_run/splitted_seq
+	fi
+
+	#Handling the input file into a cutadapt file and a mapping pipeline file
+	## finding the used restriction sites to generate a fragment map.
+	RES1=$(cut -f4 $input | tail -1)
+	RES2=$(cut -f5 $input | tail -1) 
+}
+##########################################################################################################################
+#The cutadapt function
+#Runs the cutadapt program based on the amount of primers included in the input files. 
+##########################################################################################################################
+function Demultiplexer() {
+	number=0
+	#Demultiplexing fastq files using cutadapt 
+	#Names the files after the primer names.
+	for inputfile in  ${data}/*.f*; do
+		number=$((${number} +1))
+		for primerseq in $(cut -f2 $input); do
+			primerseq=$(echo $primerseq | sed 's/GATC//g')
+			Primerlength=$(echo $primerseq| wc -m) 
+			cutadapt -O 13 -g $primerseq -o '{name}' $inputfile 
+			PrimerName=$(grep ${primerseq} $input | cut -f1)
+			mv 1 ${output}/test_run/splitted_seq/${PrimerName}${number}.fastq
+			cp ${output}/test_run/splitted_seq/${PrimerName}${number}.fastq ${output}/
+		done
+	done
+	echo "Demultiplex check, so far so good."
+}
+##########################################################################################################################
+#Aligning and alignment prep.
+##########################################################################################################################
+function AllTheOtherStuff() {
+	#Calculates readlength without primers but including the restriction site.
+	PrimerLength=$(awk 'NR==1' $input | cut -f2 | wc -m)
+	ActualPrimerLength=$(echo $((${lengthofread}-${Primerlength} + 4)))
+	  
+	#prep fragment map
+	#check if fragment map exists
+	if [ ! -d "${currentlocation}/fragment_map" ];
+	then
+		perl ${currentlocation}/generate_fragment_map.pl ${PathToReferenceGenome}ucsc.hg19.fa ${RES1} ${RES2} ${currentlocation}/fragment_map/
+		echo "Fragment map has been succesfully made!"
+	else
+		echo "Fragment map already exists!"
+	fi
+
+
+	#check if repeats are already made
+	if [ ! -d "${currentlocation}/test_repeat/${ActualPrimerLength}" ];
+	then 
+		perl ${currentlocation}/getRepeats.pl ${currentlocation}/fragment_map/ ${RES1} ${ActualPrimerLength} ${PathToReferenceGenome}ucsc.hg19.fa ${currentlocation}/test_repeat/
+		echo "Repeats for the ${ActualPrimerLength} are made!"
+	else
+		echo "Repeats for the ${ActualPrimerLength} length already exist!"
+	fi
+
+	#Creates a file for each fastq file that numbers the input files for analysis.
+	NumberofInputfiles=$(ls ${currentlocation}/inputfiles/ | wc -l) 
+	for Amount in $(seq ${NumberofInputfiles}); do
+		awk -v var="$Amount" '$1=$1var' ${input} > ${input}${Amount}.txt
+		sed -i 's/ /\t/g' ${input}${Amount}.txt
+	done
+	echo "Start of alignment!"
+
+	#Runs the mapping pipeline to map the fastq data.
+	numberoffiles=0
+	for f in ${currentlocation}/inputfiles/*; do
+		numberoffiles=$((numberoffiles+1))
+		perl ${currentlocation}/mapping_pipeline.pl ${input}${numberoffiles}.txt ${output}/test_run $f 10 ${currentlocation}/fragment_map/ ${currentlocation}/test_repeat/${ActualPrimerLength}	
+		rm ${input}${numberoffiles}.txt
+	done
+	rm ${currentlocation}/unknown
+
+}
+
+
+
+##################################################################################################################
+#Function that runs the Rscript and does the transcription factor prioritization.
+#Does the peakC analysis and performs the prioritization per viewpoint.
+#
+##################################################################################################################
+function RandAnalysis() {
+	echo "Rscriptstart"
+	#PeakC
+	###Initializing R
+	module load RPlus
+	export R_LIBS=~/Rlibs
+	for viewpoint in $(cut -f1 ${input}); do
+
+		Pattern=${viewpoint}
+		viewpointnumber=$(grep ${viewpoint} ${input} | cut -f7)
+		viewpointfile=${output}/test_run/filtered_file/*${viewpoint}*
+		#creates an output file for each viewpoint
+		outputfile=${output}/${viewpoint}
+		if [ ! -d "${outputfile}" ];
+		then
+			mkdir ${outputfile}
+		fi
+		Rscript ${currentlocation}/PeakC.R $viewpoint $viewpointnumber $outputfile/ ${output}/test_run/filtered_file/ ${Pattern} ${outputfile}/${Pattern}.txt
+		mv $outputfile/Plot.jpg $outputfile/${Pattern}.jpg
+	
+
+		Jasparinput=${outputfile}/${viewpoint}.txt
+		NameofInputFile=${inputname}
+		filename=$(basename -- "$Jasparinput")
+		InputnameNoExtention="${filename%.*}"
+		chromosomenumber=$(grep $InputnameNoExtention ${input} | cut -f6)
+		NumberofPeaks=0
+		sed -i '/x/d' $Jasparinput
+		if [ -s $Jasparinput ]; 
+		then 
+			coordinate2=0
+			for coordinate in $(cat $Jasparinput); do
+	 			coordinate1=$coordinate
+				ComparedCoordinate=$(($coordinate1 - $coordinate2))
+				if [ "${ComparedCoordinate}" -ge "30000" ];
+				then
+	                		NumberofPeaks=$(($NumberofPeaks+1))
+	        		fi
+	        		echo $coordinate >> ${Jasparoutput}/${InputnameNoExtention}${NumberofPeaks}.txt
+	        		coordinate2=$coordinate1
+			done
+	
+			#for numbers in peak file take highest and lowest
+			#Create a running window of 300 bp
+
+			for peak in ${Jasparoutput}/${InputnameNoExtention}*; do
+				echo $peak
+		        	peakername=$(basename -- "$peak")
+		        	PeakName="${peakername%.*}"
+		        	start=$(cat $peak | head -1)
+		        	end=$(cat $peak | tail -1)
+		        	number1=$start
+		        	while [ "$number1" -lt "$end" ]; do
+		        	        number2=$(($number1+${Window}))
+		        	        echo "${chromosomenumber}       ${number1}      ${number2}      ${PeakName}" >> ${Jasparoutput}/${InputnameNoExtention}.bed
+		        	        number1=$((${number1}+${LengthofWindow}))
+		        	done
+			done
+
+			#Compare Jaspar database using bedtools
+			for f in ${Jaspardatafile}MA*; do
+			        MatrixName=$(basename -- "$f")
+			        MatrixNameNoExtention="${MatrixName%.*}"
+				sed -i 's/ \+ /\t/g' ${Jasparoutput}/${InputnameNoExtention}.bed
+			        bedtools intersect -wa -a ${Jasparoutput}/${InputnameNoExtention}.bed -wb -b $f > ${Jasparoutput}/${MatrixNameNoExtention}.tmp
+			        TranscriptionFactor=$(awk 'NR==5' ${JasparMatrixInfo}${MatrixNameNoExtention}.jaspar)
+			        sed -i "s/$/\t${TranscriptionFactor}/" ${Jasparoutput}/${MatrixNameNoExtention}.tmp
+			done
+			cat ${Jasparoutput}/*.tmp >> ${Jasparoutput}/${InputnameNoExtention}_output.bed
+			sed -i '/hg38/d' ${Jasparoutput}/${InputnameNoExtention}_output.bed
+			sort ${Jasparoutput}/${InputnameNoExtention}_output.bed > tmp && mv tmp ${Jasparoutput}/${InputnameNoExtention}_output.bed
+			rm ${Jasparoutput}/*.tmp
+			rm ${Jasparoutput}/*.txt
+		fi	
+	done
+}
+###########################################################################################################
+#Installs Rlibrary, and packages
+###########################################################################################################
+function Rprep() {
+	module load RPlus
+	mkdir ~/Rlibs
+        Rscript -e "install.packages('devtools', '~/Rlibs', 'http://ftp.ussg.iu.edu/CRAN')"
+        Rscript -e "install.packages('isotone', '~/Rlibs', 'http://ftp.ussg.iu.edu/CRAN')"
+        Rscript -e "install.packages('caTools', '~/Rlibs', 'http://ftp.ussg.iu.edu/CRAN')"
+        Rscript -e "devtools::install_github('deWitLab/peakC')"
+}
+###########################################################################################################
+
+
+
+#main
+while getopts "i:h:wlr:" opt;
+do
+        case $opt in h)showHelp;; i)input="${OPTARG}";; w)Window="${OPTARG}";; l)LengthofWindow="${OPTARG}";; r)RPrep;;
+        esac
+done
+if [[ -z "${input:-}" ]]; then showHelp ; echo "No input is given" ; fi
+if [[ -z "${Window:-}" ]]; then Window="300" ; fi
+if [[ -z "${LengthofWindow:-}" ]]; then LengthofWindow="50" ; fi
+
+
+
+
+
+#Running the program. 
+
+Prepwork
+Demultiplexer
+AllTheOtherStuff
+RandAnalysis
+
+
+echo "All done, have a nice day!"
+
--- PeakC.R
+++ PeakC.R
@@ -1,273 +1,34 @@
+#!/usr/bin/Rscript
 library(peakC)
-data <- readMatrix("C:/4C/peakC/Gecombineerdefiles/BAG3_1.txt", vp.pos= 121416630, window = 700e3)
-res <- single.analysis(data$data, vp.pos=121416630, qWd=2.5)
-plot_C(res)
-title(main="BAG3")
-
-data <- readMatrix("C:/4C/peakC/Gecombineerdefiles/BAG3_2.txt", vp.pos= 121416630, window = 700e3)
-res <- single.analysis(data$data, vp.pos=121416630, qWd=2.5)
-plot_C(res)
-title(main="BAG3")
-
-data <- readMatrix("C:/4C/peakC/Gecombineerdefiles/BAG3_3.txt", vp.pos= 121416630, window = 700e3)
-res <- single.analysis(data$data, vp.pos=121416630, qWd=2.5)
-plot_C(res)
-title(main="BAG3")
+args <- commandArgs(trailingOnly = TRUE)
+ViewpointGene <- args[1]
+viewpoint <- args[2]
+Outputfile <- args[3]
+FileDirectory <- args[4]
+Pattern <- args[5]
+Outputtxt <- args[6]
+Outputtxt 
 
 
-viewpoint <- 121416630
-files <- dir(path="C:/4C/peakC/Gecombineerdefiles/", pattern="BAG3_[123].txt", full=TRUE)
+
+
+
+#Pattern
+
+viewpoint <- as.numeric(viewpoint)
+
+
+
+files <- dir(path=FileDirectory, pattern=Pattern, full=TRUE)
 data <- readMultiple(files, vp.pos=viewpoint)
 res <- combined.analysis(data, vp.pos=viewpoint)
+write.table(res$peak, Outputtxt, sep ="\t", dec=".", row.names = F, col.names = T, append=T)
+setwd(Outputfile)
+jpeg('Plot.jpg')
 plot_C(res)
-title(main="BAG3 with replicates")
-```
-```{r single experiment}
-data <- readMatrix("C:/4C/peakC/Gecombineerdefiles/TTN_1.txt", vp.pos= 179673573, window = 700e3)
-res <- single.analysis(data$data, vp.pos=179673573, qWd=2.5)
-plot_C(res)
-title(main="TTN")
 
-data <- readMatrix("C:/4C/peakC/Gecombineerdefiles/TTN_2.txt", vp.pos= 179673573, window = 700e3)
-res <- single.analysis(data$data, vp.pos=179673573, qWd=2.5)
-plot_C(res)
-title(main="TTN")
-
-data <- readMatrix("C:/4C/peakC/Gecombineerdefiles/TTN_3.txt", vp.pos= 179673573, window = 700e3)
-res <- single.analysis(data$data, vp.pos=179673573, qWd=2.5)
-plot_C(res)
-title(main="TTN")
+dev.off() 
 
 
-viewpoint <- 179673573
-files <- dir(path="C:/4C/peakC/Gecombineerdefiles/", pattern="TTN_[123].txt", full=TRUE)
-data <- readMultiple(files, vp.pos=viewpoint)
-res <- combined.analysis(data, vp.pos=viewpoint)
-plot_C(res)
-title(main="TTN with replicates")
-```
 
 
-```{r Combined data}
-library(peakC)
-viewpoint <- 121416630
-files <- dir(path="C:/4C/peakC/Gecombineerdefiles/", pattern="BAG3_[123].txt", full=TRUE)
-data <- readMultiple(files, vp.pos=viewpoint)
-res <- combined.analysis(data, vp.pos=viewpoint)
-head(res$peak)
-plot_C(res)
-title(main="BAG3")
-
-viewpoint2 <- 179673573
-files2 <- dir(path="C:/4C/peakC/Gecombineerdefiles/", pattern="TTN_[123].txt", full=TRUE)
-data2 <- readMultiple(files2, vp.pos=viewpoint2)
-res2 <- combined.analysis(data2, vp.pos=viewpoint2)
-head(res2$peak)
-plot_C(res2)
-title(main="TTN")
-
-viewpoint3 <- 201346777
-files3 <- dir(path="C:/4C/peakC/Gecombineerdefiles/", pattern="TTN2_[123].txt", full=T)
-data3 <- readMultiple(files3, vp.pos=viewpoint3)
-res3 <- combined.analysis(data3, vp.pos=viewpoint3)
-head(res3$peak)
-plot_C(res3)
-title(main="TNNT2")
-
-viewpoint4<- 201341573
-files4 <- dir(path="C:/4C/peakC/Gecombineerdefiles/", pattern="TTN2_2_[123].txt", full=TRUE)
-data4 <- readMultiple(files4, vp.pos=viewpoint4)
-res4 <- combined.analysis(data4, vp.pos=viewpoint4)
-head(res4$peak)
-plot_C(res4)
-title(main="TNNT2_2")
-
-viewpoint5 <- 23865593
-files5 <- dir(path="C:/4C/peakC/Gecombineerdefiles/", pattern="MYH6_[123].txt", full=TRUE)
-data5 <- readMultiple(files5, vp.pos=viewpoint5)
-res5 <- combined.analysis(data5, vp.pos=viewpoint5)
-head(res5$peak)
-plot_C(res5)
-title(main="MYH6")
-
-
-viewpoint6 <- 23901458
-files6 <- dir(path="C:/4C/peakC/Gecombineerdefiles/", pattern="MYH7_[123].txt", full=TRUE)
-data6 <- readMultiple(files6, vp.pos=viewpoint6)
-res6 <- combined.analysis(data6, vp.pos=viewpoint6)
-head(res6$peak)
-plot_C(res6, y.max = 5000)
-title(main="MYH7")
-
-viewpoint8 <- 156052727
-files8 <- dir(path="C:/4C/peakC/Gecombineerdefiles/", pattern="LMNA1_[123].txt", full=TRUE)
-data8 <- readMultiple(files8, vp.pos=viewpoint8)
-res8 <- combined.analysis(data8, vp.pos=viewpoint8)
-head(res8$peak)
-plot_C(res8)
-title(main="LMNA1")
-
-viewpoint7 <- 156086255
-files7 <- dir(path="C:/4C/peakC/Gecombineerdefiles/", pattern="LMNA2_[123].txt", full=TRUE)
-data7 <- readMultiple(files7, vp.pos=viewpoint7)
-res7 <- combined.analysis(data7, vp.pos=viewpoint7)
-head(res7$peak)
-plot_C(res7)
-title(main="LMNA2")
-
-
-#### 
-#### 20 mei concept verslag
-#### verslag begin juni
-######Afstuderen 20 juni half 1 presentatie - kwart over 1
-```
-
-
-```{r }
-
-library(peakC)
-viewpoint <- 121416630
-files <- dir(path="C:/4C/peakC/Gecombineerdefiles/", pattern="BAG3_[123].txt", full=TRUE)
-data <- readMultiple(files, vp.pos=viewpoint)
-res <- combined.analysis(data, vp.pos=viewpoint)
-plot_C(res)
-title(main="BAG3 peakC")
-
-for (peaks in c(121357410,	121367756,	121403361,	121406094,	121407472,	121412863,	121413999, 121356399,	121367193,	121402787,	121405331,	121407302,121412102,	121413219)){
-  FourCpeakinPeakC <- which(abs(res$dbR$frag_pos-peaks)==min(abs(res$dbR$frag_pos-peaks)))
-  ActualPeakCpos <- c(ActualPeakCpos, res$dbR$frag_pos[FourCpeakinPeakC])
-  
-}
-
-res$peak <- ActualPeakCpos
-plot_C(res)
-title(main="BAG3 FourCSeq")
-
-######################################################################################################################
-viewpoint2 <- 179673573
-files2 <- dir(path="C:/4C/peakC/Gecombineerdefiles/", pattern="TTN_[123].txt", full=TRUE)
-data2 <- readMultiple(files2, vp.pos=viewpoint2)
-res2 <- combined.analysis(data2, vp.pos=viewpoint2)
-plot_C(res2)
-title(main="TTN PeakC")
-
-#fourCpeaks <- c(179661092, 179685470, 179698789, 179702794, 179727086, 179729901, 179733185, 179741176, 179741528, 179745434, 179775810, 179785901)
-
-for (peaks in c(179664586,	179664888,	179672190, 179673064, 179685470,	179698789,	179699999,	179702794,	179709915,	179720513,	179727086,	179729008,	179729901,	179730323,	179733185,	179741176,	179741528,	179745434,	179752851, 179664883,	179666902,	179673059,	179673587,	179685776,	179699367,	179700732,	179703358,	179710840,	179720777,	179727692,	179729887,	179730318,	179731515,	179734483,	179741523,	179741822,	179746531,	179752958)){
-  FourCpeakinPeakC <- which(abs(res2$dbR$frag_pos-peaks)==min(abs(res2$dbR$frag_pos-peaks)))
-  ActualPeakCpos <- c(ActualPeakCpos, res2$dbR$frag_pos[FourCpeakinPeakC])
-  
-}
-
-res2$peak <- ActualPeakCpos
-plot_C(res2)
-title(main="TTN FourCSeq")
-######################################################################################################################
-
-viewpoint3 <- 201346777
-files3 <- dir(path="C:/4C/peakC/Gecombineerdefiles/", pattern="TTN2_[123].txt", full=T)
-data3 <- readMultiple(files3, vp.pos=viewpoint3)
-res3 <- combined.analysis(data3, vp.pos=viewpoint3)
-plot_C(res3)
-title(main="TNNT2 peakC")
-
-for (peaks in c()){
-  FourCpeakinPeakC <- which(abs(res3$dbR$frag_pos-peaks)==min(abs(res3$dbR$frag_pos-peaks)))
-  ActualPeakCpos <- c(ActualPeakCpos, res3$dbR$frag_pos[FourCpeakinPeakC])
-  
-}
-
-res3$peak <- ActualPeakCpos
-plot_C(res3)
-title(main="TNNT2 FourCSeq")
-
-######################################################################################################################
-
-viewpoint4<- 201341573
-files4 <- dir(path="C:/4C/peakC/Gecombineerdefiles/", pattern="TTN2_2_[123].txt", full=TRUE)
-data4 <- readMultiple(files4, vp.pos=viewpoint4)
-res4 <- combined.analysis(data4, vp.pos=viewpoint4)
-plot_C(res4)
-title(main="TNNT2_2 PeakC")
-
-for (peaks in c(201336079,	201338149,	201339288,	201345697,	201337780,	201338717,	201339740,	201346772)){
-  FourCpeakinPeakC <- which(abs(res4$dbR$frag_pos-peaks)==min(abs(res4$dbR$frag_pos-peaks)))
-  ActualPeakCpos <- c(ActualPeakCpos, res4$dbR$frag_pos[FourCpeakinPeakC])
-  
-}
-res4$peak <- ActualPeakCpos
-plot_C(res4, y.max = 3000)
-title(main="TNNT2_2 FourCSeq")
-
-######################################################################################################################
-
-viewpoint5 <- 23865593
-files5 <- dir(path="C:/4C/peakC/Gecombineerdefiles/", pattern="MYH6_[123].txt", full=TRUE)
-data5 <- readMultiple(files5, vp.pos=viewpoint5)
-res5 <- combined.analysis(data5, vp.pos=viewpoint5)
-plot_C(res5)
-title(main="MYH6 PeakC")
-
-for (peaks in c(23864623,	23867467,	23870458,	23873236,	23901694,	23904226,	23904489,	23907602,	23911007,	23914591,	23925331,	23864798,	23868026,	23871112, 23873576,	23902077,	23904484,	23907056,	23908184,	23913075,	23915127,	23925645)){
-  FourCpeakinPeakC <- which(abs(res5$dbR$frag_pos-peaks)==min(abs(res5$dbR$frag_pos-peaks)))
-  ActualPeakCpos <- c(ActualPeakCpos, res5$dbR$frag_pos[FourCpeakinPeakC])
-  
-}
-res5$peak <- ActualPeakCpos
-plot_C(res5, y.max = 3000)
-title(main="MYH6 FourCSeq")
-######################################################################################################################
-
-viewpoint6 <- 23901458
-files6 <- dir(path="C:/4C/peakC/Gecombineerdefiles/", pattern="MYH7_[123].txt", full=TRUE)
-data6 <- readMultiple(files6, vp.pos=viewpoint6)
-res6 <- combined.analysis(data6, vp.pos=viewpoint6)
-plot_C(res6, y.max = 3000)
-title(main="MYH7 PeakC")
-
-
-for (peaks in c(23876080,	23902082,	23926758,	23877160,	23902260,	23927321)){
-  FourCpeakinPeakC <- which(abs(res6$dbR$frag_pos-peaks)==min(abs(res6$dbR$frag_pos-peaks)))
-  ActualPeakCpos <- c(ActualPeakCpos, res6$dbR$frag_pos[FourCpeakinPeakC])
-  
-}
-res6$peak <- ActualPeakCpos
-plot_C(res6, y.max = 3000)
-title(main="MYH7 FourCSeq")
-
-######################################################################################################################
-viewpoint7 <- 156052727
-files7 <- dir(path="C:/4C/peakC/Gecombineerdefiles/", pattern="LMNA1_[123].txt", full=TRUE)
-data7 <- readMultiple(files7, vp.pos=viewpoint7)
-res7 <- combined.analysis(data7, vp.pos=viewpoint7)
-plot_C(res7)
-title(main="LMNA1 PeakC")
-
-
-for (peaks in c(156034859,	156038825,	156041749,	156055198,	156092798,	156101547,	156103541,	156103875,	156190661,	156193112,	156035708,	156039035,	156042805,	156056384,	156093829,	156101736,	156103870,	156104847,	156191555,	156193644)){
-  FourCpeakinPeakC <- which(abs(res7$dbR$frag_pos-peaks)==min(abs(res7$dbR$frag_pos-peaks)))
-  ActualPeakCpos <- c(ActualPeakCpos, res7$dbR$frag_pos[FourCpeakinPeakC])
-  
-}
-res7$peak <- ActualPeakCpos
-plot_C(res7, y.max = 3000)
-title(main="LMNA1 FourCSeq")
-
-
-######################################################################################################################
-viewpoint8 <- 156086255
-files8 <- dir(path="C:/4C/peakC/Gecombineerdefiles/", pattern="LMNA2_[123].txt", full=TRUE)
-data8 <- readMultiple(files8, vp.pos=viewpoint8)
-res8 <- combined.analysis(data8, vp.pos=viewpoint8)
-plot_C(res8)
-title(main="LMNA2 PeakC")
-
-for (peaks in c(156055198,	156058836,	156061590,	156063165,	156063339,	156063783,	156065763,	156066068,	156066920,	156067167,	156069801,	156070767,	156077936,	156078239,	156079746,	156080547,	156081986,	156089149,	156089985,	156090948,	156091726,	156092798,	156093834,	156098438,	156101547,	156103541,	156166266,	156190661,	156056384,	156059005,	156062118,	156063310,	156063530,	156063927,	156066063,	156066915,	156067162,	156067759,	156070599,	156070986,	156078234,	156078340,	156080379, 156081333,	156082127,	156089843,	156090056,	156091418,	156091846,	156093829,	156094142,	156099384,	156101736,	156103870,	156166939,	156191555)){
-  FourCpeakinPeakC <- which(abs(res8$dbR$frag_pos-peaks)==min(abs(res8$dbR$frag_pos-peaks)))
-  ActualPeakCpos <- c(ActualPeakCpos, res8$dbR$frag_pos[FourCpeakinPeakC])
-  
-}
-res8$peak <- ActualPeakCpos
-plot_C(res8, y.max = 3000)
-title(main="LMNA2 FourCSeq")
-######################################################################################################################
