a3448ceb818c762bc98a3e62a5085b251c4a281a
Hidding <i.j.hidding@umcg.nl>
2019-07-30 09:29:06 +0100

    removed obsolete files

--- TFcheckonregionscript.sh
+++
@@ -1,6 +0,0 @@
-Jaspardatafile=/groups/umcg-gdio/tmp04/umcg-ihidding/IWAN/Jasparbed/
-
-for f in ${Jaspardatafile}MA*; do
-	echo $f >> TFSoutputinrgion.txt
-	bedtools intersect -wb -b $1 -wa -a $f >> TFSoutputinrgion.txt
-done
